5205ac527ba7d7913a2c3825972d66e096d5c6a9
Hayashi <k014c1105@it-neec.jp>
2016-05-30 11:10:59 +0900

    change 4rth line

--- db.sh
+++ db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /opt/lampp/bin
 ./mysql -uroot -e "drop database todoDB;"
-./mysql -uroot -e  "create database todoDB;"
+./mysql -uroot -e  "create database todoDB char set utf8;"
 ./mysql -uroot todoDB -e "create table ToDoList(id int not null auto_increment primary key,text varchar(255));"
