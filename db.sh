021b1f449736d2de2aa81c42bccaf4a7265fb55f
Hayashi <k014c1105@it-neec.jp>
2016-05-30 11:22:30 +0900

    2lines comment out

--- db.sh
+++ db.sh
@@ -3,5 +3,5 @@ cd /opt/lampp/bin
 ./mysql -uroot -e "drop database todoDB;"
 ./mysql -uroot -e  "create database todoDB char set utf8;"
 ./mysql -uroot todoDB -e "create table ToDoList(id int not null auto_increment primary key,text varchar(255));"
-./mysql -uroot todoDB -e "insert into ToDoList(text) value('testdata');"
-./mysql -uroot todoDB -e "select * from ToDoList;"
+# ./mysql -uroot todoDB -e "insert into ToDoList(text) value('testdata');"
+# ./mysql -uroot todoDB -e "select * from ToDoList;"
