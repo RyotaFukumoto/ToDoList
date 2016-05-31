647809ad3bafb3c40f45839ff6e3c94ef91f9da6
mochizuki <k014c1121@it-neec.jp>
2016-05-31 14:03:38 +0900

    index.php

--- index.php
+++ index.php
@@ -1,39 +1,46 @@
 <?php
 
-try{
-$dsn = 'mysql:dbname=todoDB;host=localhost;charset=utf8';
-$user = 'root';
-$password = '';
+$errors = array();
+$text = "aaa";
 
-$dbh = new PDO($dsn, $user, $password);
-$dbh->query('SET NAMES utf8');
-$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-print('接続できてる');
-$db = NULL;
+try{
+  $dsn = 'mysql:dbname=todoDB;host=localhost;charset=utf8';
+  $user = 'root';
+  $password = '';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  print('接続できてます。');
+  $db = NULL;
 }catch(PDOException $e){
-  die('エラー')
+  die('エラー');
 }
 
-
 if(isset($_GET['add'])){
-    $text = $_GET['memo'];
-    $text = htmlspecialchars($text, ENT_QUOTES);
+  $text = $_GET['memo'];
+  $text = htmlspecialchars($text, ENT_QUOTES);
 
-        $sql = 'INSERT INTO ToDoList (text) VALUE($text)';
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(1, $text, PDO::PARAM_STR);
-
-        $stmt->execute();
-
-        $dbh = null;
-
-        unset($text);
-    }else if(isset($_GET['delete'])){
-    }
+  if($text === ''){
+      $errors['text'] = '予定が入力されていません。';
+      print $errors["text"];
+  }else{
 
 
-
-
+  $sql = 'INSERT INTO ToDoList (text) VALUE(:text)';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(':text', $text, PDO::PARAM_STR);
+  $stmt->execute();
+  $dbh = null;
+  unset($text);
+}
+}else if(isset($_GET['delete'])){
+  $num =$_GET['delete'];
+  // $sql 'DELETE FROM ToDoList WHERE id = :num';
+  $sql = 'DELETE FROM ToDoList WHERE id = :num';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(':num',$num,PDO::PARAM_INT);
+  $stmt->execute();
+}
 ?>
 
 
@@ -41,17 +48,49 @@ if(isset($_GET['add'])){
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>PHPサンプル</title>
+<title>ToDoList</title>
+<link href="http://netdna.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.css" rel="stylesheet">
+
 </head>
 <body>
   <div class="container">
-  <form action="" method="GET">
-  <input type="text"id="memo" name="memo" value="" />
-</div>
-  <input type="submit" name="add" id="add" value="追加" />
+    <form action="" method="GET">
+    <input type="text"id="memo" name="memo" value="" />
+    <input type="submit" name="add" id="add" value="追加" />
+
+  </div>
 </form>
 <HR>
+<?php
 
+
+
+try{
+  $dsn = 'mysql:dbname=todoDB;host=localhost;charset=utf8';
+  $user = 'root';
+  $password = '';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}catch(PDOException $e){
+  die('エラー');
+}
+  $sql = 'select id, text from ToDoList;';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+  // $dbh = null;
+  while($task = $stmt->fetch(PDO::FETCH_ASSOC)){
+    echo "<div class='container'>";
+    echo "<form method='get' sction='index.php'>".$task['text']."  ";
+    echo "<button type='submit' name='delete' value='".$task['id']."'><i class='fa fa-trash-o' aria-hidden='true'></i></button>";
+    echo "</form></div>";
+
+
+  }
+
+
+
+?>
 
 </body>
 </html>
